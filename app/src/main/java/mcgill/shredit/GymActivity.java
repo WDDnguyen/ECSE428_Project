9da574e83a69409fd48f4e577c38f0bf1e3c5185
William Nguyen <WDDnguyen@gmail.com>
2019-02-15 17:10:02 -0500

    added few functions for gymActivity to be completed

--- app/src/main/java/mcgill/shredit/GymActivity.java
+++ app/src/main/java/mcgill/shredit/GymActivity.java
@@ -3,11 +3,38 @@ package mcgill.shredit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
+import mcgill.shredit.model.Gym;
+
 public class GymActivity extends AppCompatActivity {
+
+    ArrayList<Gym> gyms;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gym);
     }
+
+    // TO DO : Create Gyms using mock data
+    public void generateGyms(){
+
+    }
+
+    // TO DO : search function based based on input given from user.
+    public void searchGym(){
+
+    }
+
+    // TO DO : Display gyms list based on the gym view design
+    public void displayGyms(){
+
+    }
+
+    // TO DO : Pass gym's equipment list to muscle group Activity
+    public void sendIntent(){
+
+    }
+
 }
