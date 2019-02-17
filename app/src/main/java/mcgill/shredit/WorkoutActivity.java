1ed5d7ebd3d41576e70abe8b3ba6c0d77c8c527e
William Nguyen <WDDnguyen@gmail.com>
2019-02-17 10:19:29 -0500

    initialize application activities and layouts

---
+++ app/src/main/java/mcgill/shredit/WorkoutActivity.java
@@ -0,0 +1,13 @@
+package mcgill.shredit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WorkoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_workout);
+    }
+}
