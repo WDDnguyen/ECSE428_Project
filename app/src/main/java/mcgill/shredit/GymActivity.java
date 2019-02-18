a150bb33125874ec530aae3b85172c85c1e9a5a8
William Nguyen <WDDnguyen@gmail.com>
2019-02-17 21:49:17 -0500

    added muscle group functionality and intent flow

--- app/src/main/java/mcgill/shredit/GymActivity.java
+++ app/src/main/java/mcgill/shredit/GymActivity.java
@@ -4,8 +4,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import mcgill.shredit.model.Equipment;
+import mcgill.shredit.model.Gym;
 
 public class GymActivity extends AppCompatActivity {
+
+    // TO DO : generate gym data; Remove this when completed.
+    Gym selectedGym = new Gym("FlexBoi", 1);
+    Equipment gymEquipment = new Equipment("treadmill", 1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +25,20 @@ public class GymActivity extends AppCompatActivity {
         setContentView(R.layout.activity_gym);
     }
 
-    // TO DO : Need to pass gym's equipment list to muscle group activity
+    // TO DO : Need to pass actual generated gym equipment data to MuscleGroupActivity
     public void onGymConfirmClick(View view) {
-        Intent intent = new Intent(this, MuscleGroupActivity.class);
-        startActivity(intent);
+        selectedGym.addEquipment(gymEquipment);
+        List<Equipment> gymEquipments = selectedGym.getEquipments();
+
+        if(!gymEquipments.isEmpty()){
+            Intent intent = new Intent(this, MuscleGroupActivity.class);
+            intent.putExtra("EQUIPMENT_LIST", (Serializable) gymEquipments);
+            startActivity(intent);
+        }else {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "A GYM HAS TO BE SELECTED",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
--- app/src/main/java/mcgill/shredit/WorkoutActivity.java
+++ app/src/main/java/mcgill/shredit/WorkoutActivity.java
@@ -5,12 +5,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+
+import mcgill.shredit.model.Equipment;
+
 public class WorkoutActivity extends AppCompatActivity {
+
+    List<Equipment> equipments;
+    HashMap<String, Integer> muscleGroups;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
+        getIntentValues();
+        printEquipments();
+        printMuscleGroups();
+    }
+
+    public void printEquipments(){
+        for (Equipment equipment : equipments){
+            System.out.println("EQUIPEMENT RECEIVED : " + equipments);
+        }
+    }
+
+    public void printMuscleGroups(){
+        for (String muscleGroup : muscleGroups.keySet()){
+            System.out.println("MUSCLE GROUP : " + muscleGroup  + " NUMBER OF EXERCISES : " + muscleGroups.get(muscleGroup));
+        }
+    }
+
+    // Get intent content from MuscleGroupActivity
+    public void getIntentValues(){
+        Intent intent = getIntent();
+        equipments = (List<Equipment>) intent.getSerializableExtra("MUSCLE_GROUP_EQUIPMENT_LIST");
+        muscleGroups = (HashMap<String, Integer>) intent.getSerializableExtra("MUSCLE_GROUPS_HASHMAP");
     }
 
     public void onWorkoutDoneClick(View view){
