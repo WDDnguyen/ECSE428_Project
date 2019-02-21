d49287ea2fb3891c55366979fb92005672b02cb0
Shi Yu Liu <shiyu-mochi@users.noreply.github.com>
2019-02-21 12:04:43 -0500

    more functions
    added: get by equipment name & get by muscle name

--- app/src/main/java/mcgill/shredit/data/MockGymEquip.java
+++ app/src/main/java/mcgill/shredit/data/MockGymEquip.java
@@ -1,9 +1,10 @@
 package mcgill.shredit.data
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class MockGymEquip {
-	public static String[] getEquipmentID(int equipmentID) throws Exception {
+	public static String[] getEquipmentByID(int equipmentID) throws Exception {
 		String csvFile = "./MockGymEquipmentData.csv";
 		String line = "";
 		BufferedReader br = new BufferedReader(new FileReader(csvFile));
@@ -11,10 +12,46 @@ public class MockGymEquip {
 		boolean idFound = false;
 		while (!idFound) {
 			line = br.readLine();
-			
+
 			// use comma as separator
 			cols = line.split(",");
-			if(Integer.parseInt(cols[0]) == equipmentID){
+			if (Integer.parseInt(cols[0]) == equipmentID) {
+				idFound = true;
+			}
+		}
+		return cols;
+	}
+
+	public static String[] getEquipmentByEquipment(String equipmentName) throws Exception {
+		String csvFile = "./MockGymEquipmentData.csv";
+		String line = "";
+		BufferedReader br = new BufferedReader(new FileReader(csvFile));
+		String[] cols = new String[3];
+		boolean idFound = false;
+		while (!idFound) {
+			line = br.readLine();
+
+			// use comma as separator
+			cols = line.split(",");
+			if (0 == cols[1].compareTo(equipmentName)) {
+				idFound = true;
+			}
+		}
+		return cols;
+	}
+
+	public static String[] getEquipmentByMuscle(String muscleName) throws Exception {
+		String csvFile = "./MockGymEquipmentData.csv";
+		String line = "";
+		BufferedReader br = new BufferedReader(new FileReader(csvFile));
+		String[] cols = new String[3];
+		boolean idFound = false;
+		while (!idFound) {
+			line = br.readLine();
+
+			// use comma as separator
+			cols = line.split(",");
+			if (0 == cols[2].compareTo(muscleName)) {
 				idFound = true;
 			}
 		}
