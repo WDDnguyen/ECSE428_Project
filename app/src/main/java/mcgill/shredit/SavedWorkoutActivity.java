43a089dc83c9695cbb72be0d516899fed98c9d9e
bendwilletts <ben.d.willetts@gmail.com>
2019-03-20 17:26:33 -0400

    Implemented list selection and added functionality for Load and Delete buttons.

--- app/src/main/java/mcgill/shredit/SavedWorkoutActivity.java
+++ app/src/main/java/mcgill/shredit/SavedWorkoutActivity.java
@@ -1,12 +1,16 @@
 package mcgill.shredit;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -20,18 +24,27 @@ public class SavedWorkoutActivity extends AppCompatActivity {
 
     ListView savedWorkoutView;
     ArrayAdapter<String> adapter;
+    int selectedPos;
+    boolean isSelected;
+    ArrayList<HashMap<String,Integer>> savedMuscleGroupList;
+    ArrayList<List<Equipment>> savedEquipmentList;
+    ArrayList<String> savedWorkoutList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved_workouts);
         savedWorkoutView = findViewById(R.id.saved_workout_view);
+        savedWorkoutView.setChoiceMode(AbsListView.CHOICE_MODE_SINGLE);
 
-        final ArrayList<String> savedWorkoutList = querySavedWorkoutNames();
-        final ArrayList<List<Equipment>> savedEquipmentList = querySavedEquipments();
-        final ArrayList<HashMap<String,Integer>> savedMuscleGroupList = querySavedMuscleGroups();
+        savedWorkoutList = querySavedWorkoutNames();
+        savedEquipmentList = querySavedEquipments();
+        savedMuscleGroupList = querySavedMuscleGroups();
 
-        adapter = new ArrayAdapter<String>(
+        selectedPos = -1;
+        isSelected = false;
+
+        adapter = new ArrayAdapter<>(
                 SavedWorkoutActivity.this,
                 android.R.layout.simple_list_item_1,
                 savedWorkoutList
@@ -41,22 +54,80 @@ public class SavedWorkoutActivity extends AppCompatActivity {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplication(), WorkoutActivity.class);
-                intent.putExtra("MUSCLE_GROUP_EQUIPMENT_LIST", (Serializable) savedEquipmentList.get(position));
-                intent.putExtra("MUSCLE_GROUPS_HASHMAP", savedMuscleGroupList.get(position));
-                startActivity(intent);
+                selectedPos = position;
+                isSelected = true;
+                savedWorkoutView.setSelection(selectedPos);
             }
         });
         savedWorkoutView.setAdapter(adapter);
     }
 
-    private ArrayList<String> querySavedWorkoutNames(){
+    public void onLoadButtonClick(View view) {
+        if(isSelected){
+            Intent intent = new Intent(this, WorkoutActivity.class);
+            intent.putExtra("MUSCLE_GROUP_EQUIPMENT_LIST", (Serializable) savedEquipmentList.get(selectedPos));
+            intent.putExtra("MUSCLE_GROUPS_HASHMAP", savedMuscleGroupList.get(selectedPos));
+            startActivity(intent);
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Please select a workout to load!",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    public void onDeleteButtonClick(View view) {
+        if(isSelected) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Are you sure you want to delete " + savedWorkoutList.get(selectedPos));
+            builder.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    String woName = savedWorkoutList.get(selectedPos);
+                    writeDeleteSavedWorkout(woName, savedEquipmentList.get(selectedPos), savedMuscleGroupList.get(selectedPos));
+                    savedWorkoutView.clearChoices();
+                    savedWorkoutList.remove(selectedPos);
+                    savedEquipmentList.remove(selectedPos);
+                    savedMuscleGroupList.remove(selectedPos);
+                    adapter.notifyDataSetChanged();
+                    savedWorkoutView.setAdapter(adapter);
+                    selectedPos = -1;
+                    isSelected = false;
+                    Toast toast = Toast.makeText(getApplicationContext(),
+                            woName + " workout deleted.",
+                            Toast.LENGTH_SHORT);
+                    toast.show();
+                    dialog.dismiss();
+
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.cancel();
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+        else {
+            Toast toast = Toast.makeText(getApplicationContext(),
+                    "Please select a workout to delete!",
+                    Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
+    }
+
+    private void writeDeleteSavedWorkout(String workoutName, List<Equipment> workoutEquipment, HashMap<String,Integer> workoutMuscleGroups) {
+
+    }
+
+    private ArrayList<String> querySavedWorkoutNames() {
         String[] array = {"Chest and Arms", "Abs"};
         ArrayList<String> savedWorkouts = new ArrayList<>(Arrays.asList(array));
         return savedWorkouts;
     }
 
-    private ArrayList<List<Equipment>> querySavedEquipments(){
+    private ArrayList<List<Equipment>> querySavedEquipments() {
         ArrayList<List<Equipment>> savedEquipments = new ArrayList<>();
         List<Equipment> e1 = Arrays.asList(new Equipment("Barbells"), new Equipment("Exercise Ball"));
         List<Equipment> e2 = Arrays.asList(new Equipment("Medicine Ball"));
@@ -65,7 +136,7 @@ public class SavedWorkoutActivity extends AppCompatActivity {
         return savedEquipments;
     }
 
-    private ArrayList<HashMap<String, Integer>> querySavedMuscleGroups(){
+    private ArrayList<HashMap<String, Integer>> querySavedMuscleGroups() {
         ArrayList<HashMap<String,Integer>> savedMuscleGroups = new ArrayList<>();
         HashMap<String,Integer> m1 = new HashMap<>();
         m1.put("Chest", 2);
